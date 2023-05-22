0585e358813e2811e519bdbba4976a4e6fd393ab
Maria Aurellia <mariaadannies26@gmail.com>
2023-05-23 02:54:50 +0700

    add urls.py, create models.py and views.py

--- pembuatan_pertandingan/views.py
+++ pembuatan_pertandingan/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Pertandingan
+
+def batalkan_peminjaman(request, waktu, stadium):
+    try:
+        pertandingan = Pertandingan.objects.get(waktu=waktu, stadium=stadium)
+        # Logika pembatalan peminjaman
+        # Misalnya, Anda dapat menghapus entri peminjaman dengan perintah pertandingan.delete()
+        return render(request, 'berhasil.html')  # Menampilkan halaman berhasil
+    except Pertandingan.DoesNotExist:
+        return render(request, 'gagal.html')  # Menampilkan halaman gagal
