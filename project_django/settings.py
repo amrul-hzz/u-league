9764b33281fce6b4370f0a2f2af6a651802cdb2a
Bellalpsb <salsabiella4@gmail.com>
2023-05-22 23:34:02 +0700

    connecting db from railway

--- project_django/settings.py
+++ project_django/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'project_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'lxJxj5QUoHdclznPAfjS',
+        'HOST': 'containers-us-west-134.railway.app',
+        'PORT': '5542',
+        'CONN_MAX_AGE': 600,
     }
 }
 
