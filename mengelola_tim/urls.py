82648f1b9001e176b8dbacd3f387d041e6030ef4
mazayanur11 <mazaya.nur11@ui.ac.id>
2023-05-23 10:43:14 +0700

    fe selesai

--- mengelola_tim/urls.py
+++ mengelola_tim/urls.py
@@ -4,8 +4,8 @@ from .views import pendaftaran_tim, tim, daftar_pemain, daftar_pelatih
 app_name = 'mengelolatim'
 
 urlpatterns = [
-    path('', pendaftaran_tim, name='pendaftaran_tim'),
-    path('tim/', tim, name='tim'),
-    path('daftarpemain/', daftar_pemain, name='daftar_pemain'),
-    path('daftarpelatih/', daftar_pelatih, name='daftar_pelatih'),
+    path('pendaftaran_tim', pendaftaran_tim, name='pendaftaran_tim'),
+    path('', tim, name='tim'),
+    path('daftar_pemain/', daftar_pemain, name='daftar_pemain'),
+    path('daftar_pelatih/', daftar_pelatih, name='daftar_pelatih'),
 ]
