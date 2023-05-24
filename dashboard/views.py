759dee19ab8841103879d4a52c634938369e9e21
mazayanur11 <mazaya.nur11@ui.ac.id>
2023-05-24 07:03:43 +0700

    dashboard manajer udh aman, tp masi manual

--- dashboard/views.py
+++ dashboard/views.py
@@ -43,10 +43,65 @@ def show_dashboard(request):
 
 def show_dashboard_manajer(request):
     cursor = connection.cursor()
+    # username_manajer = request.session("username")
+    cursor.execute(f"""
+    SELECT id_manajer
+    FROM MANAJER
+    WHERE username = 'vdeantoni15'
+    """)
+
+    id_manajer = str(cursor.fetchone()[0])
+
     cursor.execute(f"""
     SELECT *
+    FROM NON_PEMAIN
+    JOIN STATUS_NON_PEMAIN ON id = id_non_pemain
+    WHERE id = '{id_manajer}'
     """)
-    return render(request, 'dashboard_manajer.html')
+
+    data_manajer = cursor.fetchone()
+
+    cursor.execute(f"""
+    SELECT nama_tim
+    FROM TIM_MANAJER
+    WHERE id_manajer = '{id_manajer}'
+    """)
+
+    nama_tim = str(cursor.fetchone()[0])
+    print(nama_tim)
+
+    cursor.execute(f"""
+    SELECT *
+    FROM PEMAIN
+    WHERE nama_tim = '{nama_tim}'
+    """)
+
+    data_pemain = cursor.fetchall()
+
+    pemain = []
+
+    indexing = 1
+
+    if data_pemain:
+        for i in data_pemain:
+            pemain.append(
+                {
+                    "nama_pemain": i[2] + " " + i[3],
+                    "indeks": indexing,
+                }
+            )
+            indexing += 1
+
+    return render(request, 'dashboard_manajer.html', {
+        "nama_depan": str(data_manajer[1]),
+        "nama_belakang": str(data_manajer[2]),
+        "no_hp": str(data_manajer[3]),
+        "email": str(data_manajer[4]),
+        "alamat": str(data_manajer[5]),
+        "status": str(data_manajer[7]),
+        "nama_tim": nama_tim,
+        "pemain": pemain,
+    })
 
 def show_dashboard_panitia(request):
     return render(request, 'dashboard_panitia.html')
