b4d33dc426e7ebcace42407cf76a9b2e4e690c82
Maria Aurellia <mariaadannies26@gmail.com>
2023-05-25 21:41:14 +0700

    update views.py

--- pembuatan_pertandingan/urls.py
+++ pembuatan_pertandingan/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import *
+from .views import get_all_wasit_tim, get_all_pertandingan, delete_pertandingan, create_pertandingan
 
 urlpatterns = [
     path('', get_all_pertandingan, name='semua_pertandingan'),
-    path('pilih_pertandingan/', select_pertandingan, name='pilih_pertandingan'),
-    path('buat_pertandingan/<str:row_id>', create_pertandingan, name='buat_pertandingan'),
-    path('delete_pertandingan/<str:row_id>', delete_pertandingan, name='delete_pertandingan'),
+    path('buat_pertandingan/<str:row_id>/', get_all_wasit_tim, name='buat_pertandingan'),
+    path('submit_pertandingan/<str:row_id>/', create_pertandingan, name='submit_pertandingan'),
+    path('delete_pertandingan/<str:row_id>/', delete_pertandingan, name='delete_pertandingan'),
 ]
--- pembuatan_pertandingan/views.py
+++ pembuatan_pertandingan/views.py
@@ -27,35 +27,15 @@ def get_all_pertandingan(request):
 
     return render(request, 'list_pertandingan.html', context)
 
-def get_all_stadium(request):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT id_stadium, nama FROM STADIUM")
-        data = cursor.fetchall()
+def get_all_wasit_tim(request, row_id):
 
-    result = []
-
-    for i in range(len(data)):
-        result.append(
-            data[i]
-        )
-
-    stadiums_json = json.dumps([str(uuid) for uuid in result])
-
-    context = {'stadiums_json': stadiums_json,
-                'stadiums': result}
-
-    # print(result)
-
-    return render(request, 'pembuatan_pertandingan.html', context)
-
-def get_all_wasit_tim(request):
     with connection.cursor() as cursor:
         cursor.execute("SELECT id, CONCAT(nama_depan, ' ', nama_belakang) FROM NON_PEMAIN WHERE id IN (SELECT id_wasit FROM WASIT)")
         data_wasit = cursor.fetchall()
 
     result_wasit = []
 
-    for i in range(len(data)):
+    for i in range(len(data_wasit)):
         result_wasit.append(
             data_wasit[i]
         )
@@ -66,49 +46,86 @@ def get_all_wasit_tim(request):
 
     result_tim = []
 
-    for i in range(len(data)):
+    for i in range(len(data_tim)):
         result_tim.append(
             data_tim[i]
         )
 
-    context = {'wasits': result_wasit,
-                'tims': result_tim}
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT id_stadium, nama FROM STADIUM")
+        data_stadium = cursor.fetchall()
 
-    # print(result)
+    result_stadium = []
+
+    for i in range(len(data_stadium)):
+        result_stadium.append(
+            data_stadium[i]
+        )
+
+    selected = []
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT stadium FROM PERTANDINGAN WHERE id_pertandingan = %s", [row_id])
+        selected_stadium = cursor.fetchall()
+        selected.append(selected_stadium)
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT id_wasit FROM WASIT_BERTUGAS WHERE id_pertandingan = %s AND posisi_wasit='Utama' ", [row_id])
+        selected_wasit_utama = cursor.fetchall()
+        selected.append(selected_wasit_utama)
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT id_wasit FROM WASIT_BERTUGAS WHERE id_pertandingan = %s AND posisi_wasit='Hakim Garis' ", [row_id])
+        selected_wasit_pembantu = cursor.fetchall()
+        selected.append(selected_wasit_pembantu)
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT nama_tim FROM TIM_PERTANDINGAN WHERE id_pertandingan = %s", [row_id])
+        selected_tim = cursor.fetchall()
+        selected.append(selected_tim)
+
+    print(selected)
+
+    context = {'wasits': result_wasit,
+                'tims': result_tim,
+                'stadiums': result_stadium,
+                'selected': selected}
 
     return render(request, 'buat_pertandingan.html', context)
 
 def delete_pertandingan(request, row_id):
     print(row_id)
-    if request.method == 'POST':
-        try:
-            with connection.cursor() as cursor:
-                print(row_id)
-                cursor.execute("DELETE FROM PERTANDINGAN WHERE id_pertandingan = %s", [row_id])
-                cursor.execute("DELETE FROM TIM_PERTANDINGAN WHERE id_pertandingan = %s", [row_id])
-                cursor.execute("DELETE FROM WASIT_PERTANDINGAN WHERE id_pertandingan = %s", [row_id])
-                cursor.execute("DELETE FROM PEMAIN_PERTANDINGAN WHERE id_pertandingan = %s", [row_id])
+    with connection.cursor() as cursor:
+        print(row_id)
+        cursor.execute("DELETE FROM PERTANDINGAN WHERE id_pertandingan = %s", [row_id])
+        cursor.execute("DELETE FROM TIM_PERTANDINGAN WHERE id_pertandingan = %s", [row_id])
+        cursor.execute("DELETE FROM WASIT_PERTANDINGAN WHERE id_pertandingan = %s", [row_id])
+        cursor.execute("DELETE FROM PEMAIN_PERTANDINGAN WHERE id_pertandingan = %s", [row_id])
+
+        return redirect ('/pembuatan_pertandingan')
         
-            return JsonResponse({'status': 'success'})
-        except Exception as e:
-            return JsonResponse({'status': 'failed'})
 
 def create_pertandingan(request, row_id):
+    print(request.POST)
     with connection.cursor() as cursor:
-        stadium = request.get['stadium-dropdown']
-        wasit_utama = request.get['wasit_utama']
-        wasit_pembantu1 = request.get['wasit_pendamping']
-        wasit_pembantu2 = request.get['wasit_pendamping2']
-        wasit_cadangan = request.get['wasit_cadangan']
-        tim1 = request.get['tim1']
-        tim2 = request.get['tim2']
+        # stadium = request.POST.get('id-stadium')
+        # wasit_utama = request.POST.get('wasit-utama')
+        # wasit_pembantu1 = request.POST.get('wasit-pembantu1')
+        # wasit_pembantu2 = request.POST.get('wasit-pembantu2')
+        # wasit_cadangan = request.POST.get('wasit-cadangan')
+        # tim1 = request.POST.get('tim1')
+        # tim2 = request.POST.get('tim2')
 
-        print(stadium, wasit_utama, wasit_pembantu1, wasit_pembantu2, wasit_cadangan, tim1, tim2)
-
-        cursor.execute("UPDATE PERTANDINGAN SET id_stadium = %s WHERE id_pertandingan = %s", [request.POST['start_datetime'], request.POST['end_datetime'], request.POST['id_stadium'], row_id])
+        stadium = request.POST['id-stadium']
+        wasit_utama = request.POST['wasit-utama']
+        wasit_pembantu1 = request.POST['wasit-pembantu1']
+        wasit_pembantu2 = request.POST['wasit-pembantu2']
+        wasit_cadangan = request.POST['wasit-cadangan']
+        tim1 = request.POST['tim1']
+        tim2 = request.POST['tim2']
         
-        return render(request, 'buat_pertandingan.html')
+        print (stadium, wasit_utama, wasit_pembantu1, wasit_pembantu2, wasit_cadangan, tim1, tim2)
 
-def select_pertandingan(request, row_id):
-    context = {'row_id': row_id}
-    return render(request, 'buat_pertandingan.html', context)
+        cursor.execute("UPDATE PERTANDINGAN SET id_stadium = %s WHERE id_pertandingan = %s", [request.POST['id-stadium'], row_id])
+        
+        return redirect ('/pembuatan_pertandingan')
