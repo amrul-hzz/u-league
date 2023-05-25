72b4deb3df7fb8f71d63496d297cd8820bbffc5c
Maria Aurellia <mariaadannies26@gmail.com>
2023-05-25 17:11:29 +0700

    merge page

--- pembuatan_pertandingan/urls.py
+++ pembuatan_pertandingan/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import get_semua_pertandingan, get_avail_stadium, get_all_wasit, delete_pertandingan
+from .views import *
 
 urlpatterns = [
-    path('semua_pertandingan/', get_semua_pertandingan, name='semua_pertandingan'),
-    path('buat_pertandingan/', get_avail_stadium, name='buat_pertandingan'),
-    path('submit_pertandingan/', get_all_wasit, name='submit_pertandingan'),
+    path('', get_all_pertandingan, name='semua_pertandingan'),
+    path('pilih_pertandingan/', select_pertandingan, name='pilih_pertandingan'),
+    path('buat_pertandingan/<str:row_id>', create_pertandingan, name='buat_pertandingan'),
     path('delete_pertandingan/<str:row_id>', delete_pertandingan, name='delete_pertandingan'),
 ]
--- pembuatan_pertandingan/views.py
+++ pembuatan_pertandingan/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import JsonResponse
 from django.db import connection
+import uuid
 import json
 
-def get_semua_pertandingan(request):
+def get_all_pertandingan(request):
     with connection.cursor() as cursor:
         cursor.execute("SELECT p.id_pertandingan, tp.nama_tim, p.start_datetime FROM PERTANDINGAN p JOIN TIM_PERTANDINGAN tp ON p.id_pertandingan=tp.id_pertandingan GROUP BY p.id_pertandingan, tp.nama_tim ORDER BY p.start_datetime ASC")
         data = cursor.fetchall()
@@ -22,56 +23,67 @@ def get_semua_pertandingan(request):
     context = {'pertandingans_json': pertandingans_json,
                 'pertandingans': result}
 
-    # print(pertandingans_json)
+    # print(result)
 
     return render(request, 'list_pertandingan.html', context)
 
-def is_panitia(id):
-    # Implementasikan logika untuk mengecek apakah user adalah panitia
-    # Misalnya, Anda dapat menggunakan perintah request.user.groups.filter(name='panitia').exists()
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM PANITIA WHERE ID = %s", [id])
-        data = cursor.fetchall()
-
-    if data != None:
-        return True
-    else:
-        return False
-
-def get_avail_stadium(request):
+def get_all_stadium(request):
     with connection.cursor() as cursor:
         cursor.execute("SELECT id_stadium, nama FROM STADIUM")
         data = cursor.fetchall()
 
     result = []
 
-    for row in data:
-        stadium_id, stadium_name = row
-        result.append((stadium_id, stadium_name))
+    for i in range(len(data)):
+        result.append(
+            data[i]
+        )
+
+    stadiums_json = json.dumps([str(uuid) for uuid in result])
+
+    context = {'stadiums_json': stadiums_json,
+                'stadiums': result}
 
     # print(result)
 
-    return render(request, 'pembuatan_pertandingan.html', {'stadiums': result})
+    return render(request, 'pembuatan_pertandingan.html', context)
 
-def get_all_wasit(request):
+def get_all_wasit_tim(request):
     with connection.cursor() as cursor:
         cursor.execute("SELECT id, CONCAT(nama_depan, ' ', nama_belakang) FROM NON_PEMAIN WHERE id IN (SELECT id_wasit FROM WASIT)")
-        data = cursor.fetchall()
+        data_wasit = cursor.fetchall()
 
-    result = []
+    result_wasit = []
 
-    for row in data:
-        id_wasit, nama = row
-        result.append((id_wasit, nama))
+    for i in range(len(data)):
+        result_wasit.append(
+            data_wasit[i]
+        )
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM TIM_PERTANDINGAN tp JOIN TIM t ON tp.nama_tim = t.nama_tim")
+        data_tim = cursor.fetchall()
+
+    result_tim = []
+
+    for i in range(len(data)):
+        result_tim.append(
+            data_tim[i]
+        )
+
+    context = {'wasits': result_wasit,
+                'tims': result_tim}
 
     # print(result)
 
-    return render(request, 'buat_pertandingan.html', {'wasits': result})
+    return render(request, 'buat_pertandingan.html', context)
 
 def delete_pertandingan(request, row_id):
+    print(row_id)
     if request.method == 'POST':
         try:
             with connection.cursor() as cursor:
+                print(row_id)
                 cursor.execute("DELETE FROM PERTANDINGAN WHERE id_pertandingan = %s", [row_id])
                 cursor.execute("DELETE FROM TIM_PERTANDINGAN WHERE id_pertandingan = %s", [row_id])
                 cursor.execute("DELETE FROM WASIT_PERTANDINGAN WHERE id_pertandingan = %s", [row_id])
@@ -80,3 +92,23 @@ def delete_pertandingan(request, row_id):
             return JsonResponse({'status': 'success'})
         except Exception as e:
             return JsonResponse({'status': 'failed'})
+
+def create_pertandingan(request, row_id):
+    with connection.cursor() as cursor:
+        stadium = request.get['stadium-dropdown']
+        wasit_utama = request.get['wasit_utama']
+        wasit_pembantu1 = request.get['wasit_pendamping']
+        wasit_pembantu2 = request.get['wasit_pendamping2']
+        wasit_cadangan = request.get['wasit_cadangan']
+        tim1 = request.get['tim1']
+        tim2 = request.get['tim2']
+
+        print(stadium, wasit_utama, wasit_pembantu1, wasit_pembantu2, wasit_cadangan, tim1, tim2)
+
+        cursor.execute("UPDATE PERTANDINGAN SET id_stadium = %s WHERE id_pertandingan = %s", [request.POST['start_datetime'], request.POST['end_datetime'], request.POST['id_stadium'], row_id])
+        
+        return render(request, 'buat_pertandingan.html')
+
+def select_pertandingan(request, row_id):
+    context = {'row_id': row_id}
+    return render(request, 'buat_pertandingan.html', context)
