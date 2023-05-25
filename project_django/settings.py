f69dba26679e0aa2b455db42ba068b00e9976c62
Amanda Nurul Izzah <amanda.nurul11@ui.ac.id>
2023-05-25 09:52:42 +0700

    add csrf

--- project_django/settings.py
+++ project_django/settings.py
@@ -153,3 +153,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if not DEBUG:
+    CSRF_TRUSTED_ORIGINS = ['https://tk4basdat-production.up.railway.app']
